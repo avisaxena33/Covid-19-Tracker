6dfe3465b99148d41b80a32ef1a4d2b8764e3483
Aviral Saxena <aviralsaxena@Avirals-MacBook-Pro.local>
2020-12-22 01:51:10 -0600

    added some data

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,37 +2,55 @@ import React, { useRef, useState, useEffect } from 'react';
 import './index.css';
 import { all_states } from "../../all_states.js";
 import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col"
 
 const Home = () => {
     const curr_us_url = 'https://api.covidtracking.com/v1/us/current.json';
 
     const fetchCurrentCountryData = async () => {
-        const response = await fetch(curr_us_url)
-        const results = await response.json()
-        setResponse(results[0])
+        const response = await fetch(curr_us_url);
+        const results = await response.json();
+        let arr_res = [];
+        for (const [key, value] of Object.entries(results[0])) {
+            arr_res.push({key, value})
+        }
+        setResponse(arr_res)
     }
 
-    const [response, setResponse] = useState({});
+    const [response, setResponse] = useState([]);
     
     useEffect(() => {
         fetchCurrentCountryData();
     }, [])
 
-    console.log(response);
     return (
         <>
             <h1>US Covid-19 Statistics</h1>
-            <Card 
-            bg='danger' 
-            border="dark" 
-            style={{ width: '18rem' }}
-            className="mb-2"
-            >
-                <Card.Header>Positive Cases</Card.Header>
-                <Card.Body>
-                    <Card.Title>{response ? response.positive : 0}</Card.Title>
-                </Card.Body>
-            </Card>
+            <Container>
+                <CardDeck style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+                {response.map((data) => {
+                            return (
+                                <Col className="container-fluid mt-4">
+                                    <Card 
+                                    key={data.key}
+                                    bg='danger' 
+                                    border="dark" 
+                                    style={{ width: '18rem' }}
+                                    className="mb-2"
+                                    >
+                                        <Card.Header>{data.key}</Card.Header>
+                                        <Card.Body>
+                                            <Card.Title>{data.value}</Card.Title>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>      
+                            )
+                })}
+                </CardDeck>
+            </Container>
         </>
     )
 }
